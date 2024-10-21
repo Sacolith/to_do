7dbed45e11892771b9eed6c11b3e18998fdfa086
AmmarSultanDev <ammarsultan1445@gmail.com>
2024-10-21 17:28:04 +0100

    feat: update home screen design

--- lib/widgets/task_card.dart
+++ lib/widgets/task_card.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 
+/// A widget that represents a task card.
 class TaskCard extends StatelessWidget {
+  /// Constructs a [TaskCard] widget.
+  ///
+  /// The [isChecked] parameter specifies whether the task is checked or not.
+  /// The [taskTitle] parameter specifies the title of the task.
   const TaskCard({super.key, required this.isChecked, required this.taskTitle});
 
+  /// Indicates whether the task is checked or not.
   final bool isChecked;
+
+  /// The title of the task.
   final String taskTitle;
 
   @override
