26ff8441df10efb961c3f6ff6a37ba045e4d4259
Sacolith <69515678+Sacolith@users.noreply.github.com>
2024-10-21 07:48:36 -0700

    created the structure of all todos

---
+++ lib/models/task.dart
@@ -0,0 +1,12 @@
+//Creates a Set of default values for a task or to do
+class TaskModel{
+ final String name;
+  final bool isCompleted;
+   const TaskModel({
+    required this.name,
+ this.isCompleted=false,
+ //"this.isCompleted=false" sets the default value of the bool to false
+ //the other values "required this.value" makes 
+  });
+
+}
