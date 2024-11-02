5d7ed0fbdd309597419f0d87cab4b3cb6569389b
AmmarSultanDev <ammarsultan1445@gmail.com>
2024-11-03 00:04:05 +0100

    chore: Update TaskCard widget to support task dismissal

--- lib/widgets/task_card.dart
+++ lib/widgets/task_card.dart
@@ -5,45 +5,79 @@ import 'package:to_do/providers/task_provider.dart';
 
 class TaskCard extends StatelessWidget {
   final TaskModel taskModel;
-  final VoidCallback onTaskDeleted; // Add this line
+  final VoidCallback onTaskDeleted;
+  final VoidCallback onTaskUndo;
 
-  const TaskCard(
-      {super.key,
-      required this.taskModel,
-      required this.onTaskDeleted}); // Modify constructor
+  const TaskCard({
+    super.key,
+    required this.taskModel,
+    required this.onTaskDeleted,
+    required this.onTaskUndo,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Dismissible(
-      key: Key(taskModel.name),
-      background: Container(color: Colors.red),
-      onDismissed: (direction) {
-        // Remove the task from the provider
-        Provider.of<TaskProvider>(context, listen: false)
-            .deleteTasks(taskModel.name);
-        // Call the callback
-        onTaskDeleted();
-        // Show a snackbar
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('${taskModel.name} dismissed')),
-        );
-      },
-      child: Card(
-        child: ListTile(
-          title: Text(
-            taskModel.name,
-            style: TextStyle(
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
+      child: Dismissible(
+        key: Key(taskModel.name),
+        background: _buildDismissibleBackground(Alignment.centerLeft),
+        secondaryBackground: _buildDismissibleBackground(Alignment.centerRight),
+        onDismissed: (direction) => _handleDismiss(context),
+        child: _buildTaskCard(context),
+      ),
+    );
+  }
+
+  void _handleDismiss(BuildContext context) {
+    final taskProvider = Provider.of<TaskProvider>(context, listen: false);
+    taskProvider.deleteTasks(taskModel.name);
+    onTaskDeleted();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('${taskModel.name} dismissed'),
+        action: SnackBarAction(
+          label: 'UNDO',
+          onPressed: () {
+            taskProvider.addTask(taskModel);
+            onTaskUndo();
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDismissibleBackground(Alignment alignment) {
+    return Container(
+      alignment: alignment,
+      decoration: BoxDecoration(
+        color: Colors.red,
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20.0),
+        child: Icon(Icons.delete, color: Colors.white),
+      ),
+    );
+  }
+
+  Widget _buildTaskCard(BuildContext context) {
+    return Card(
+      color: Colors.transparent,
+      child: ListTile(
+        title: Text(
+          taskModel.name,
+          style: TextStyle(
               decoration:
-                  taskModel.isCompleted ? TextDecoration.lineThrough : null,
-            ),
-          ),
-          trailing: Checkbox(
-            activeColor: Colors.lightBlueAccent,
-            value: taskModel.isCompleted,
-            onChanged: (bool? value) {
-              // Handle checkbox change
-            },
-          ),
+                  taskModel.isCompleted ? TextDecoration.lineThrough : null),
+        ),
+        trailing: Checkbox(
+          activeColor: Colors.deepOrangeAccent,
+          value: taskModel.isCompleted,
+          onChanged: (bool? value) {
+            Provider.of<TaskProvider>(context, listen: false)
+                .toggleTaskCompletion(taskModel.name);
+          },
         ),
       ),
     );
